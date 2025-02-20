31ebd948d51bd5e2d2a7767c07e7564eaeb98f09
harshx1427 <harshx14@gmail.com>
2025-02-19 20:46:51 -0500

    Updated file and directory

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def home():
+    return render_template('index.html', color='green')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
